11f36efcaf4966af7d5b118d1d4232c76a47e1d0
Compute165 <73731452+Compute165@users.noreply.github.com>
2021-01-15 19:06:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var canvas = new fabric.canvas('myCanvas')
+var canvas = new fabric.Canvas('myCanvas');
 
 var partH = 30;
 var partW = 30;
@@ -24,8 +24,8 @@ function playerDraw() {
     });
 }
 
-function newImage() {
-    fabric.Image.fromURL('', function(img) {
+function newImage(getImage) {
+    fabric.Image.fromURL(getImage, function(img) {
         partObj = img;
 
         partObj.scaleToWidth(partW);
@@ -37,4 +37,72 @@ function newImage() {
 
         canvas.add(partObj);
     });
+}
+
+window.addEventListener("keydown", keyDown);
+
+function keyDown(e) {
+    key = e.keyCode;
+
+    if (e.shiftKey == true && key == '77') {
+        console.log("mshift");
+        partW = partW + 10;
+        partH = partH + 10;
+        document.getElementById("myW").innerHTML = partW;
+        document.getElementById("myH").innerHTML = partH;
+    }
+
+    if (e.shiftKey == true && key == '78') {
+        console.log("mshift");
+        partW = partW - 10;
+        partH = partH - 10;
+        document.getElementById("myW").innerHTML = partW;
+        document.getElementById("myH").innerHTML = partH;
+    }
+
+    if (key == '84') {
+        newImage('thor_face.png');
+        console.log("t");
+    }
+
+    if (key == '89') {
+        newImage('hulkd_body.png');
+        console.log("y");
+    }
+
+    if (key == '85') {
+        newImage('cap-leftArm.png');
+        console.log("u");
+    }
+
+    if (key == '73') {
+        newImage('ironman_right_hand.png');
+        console.log("i");
+    }
+
+    if (key == '79') {
+        newImage('spiderman_legs.png');
+        console.log("o");
+    }
+
+    if (key == '87') {
+        up();
+        console.log("up");
+    }
+
+    if (key == '83') {
+        down();
+        console.log("down");
+    }
+
+    if (key == '68') {
+        right();
+        console.log("right");
+    }
+
+    if (key == '65') {
+        left();
+        console.log("left");
+    }
+
 }
